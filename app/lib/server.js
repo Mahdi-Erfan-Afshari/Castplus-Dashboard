627c10afda9d12206cdfa5949e402488e04bbcc3
Mahdi-Erfan-Afshari <mahdiafshar2413@gmail.com>
2024-03-18 19:30:18 +0330

    update

--- app/lib/server.js
+++ app/lib/server.js
@@ -1,2 +1,2 @@
 const dev = process.env.NODE_ENV !== 'production'
-export const server = dev ? 'http://localhost:3000' : 'https://dashboard-castplus.vercel.app/'
+export const server = dev ? 'http://localhost:3000' : 'https://castplus.vercel.app/'
