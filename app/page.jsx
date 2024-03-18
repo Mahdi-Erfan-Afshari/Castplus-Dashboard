03743cfa1dfb6c2457ef4e74d794e4da14bbdf54
Mahdi-Erfan-Afshari <mahdiafshar2413@gmail.com>
2024-03-18 19:40:02 +0330

    update

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,15 @@
-import DashboardPage from "./components/Dashboard"
+// import DashboardPage from "./components/Dashboard"
+import dynamic from 'next/dynamic';
+
+const DynamicDashboardPage = dynamic(
+	() => import('./components/Dashboard'),
+	{ ssr: false }
+);
 
 export default function Home() {
 	return (
 		<main>
-			<DashboardPage />
+			<DynamicDashboardPage />
 		</main>
 	)
 }
