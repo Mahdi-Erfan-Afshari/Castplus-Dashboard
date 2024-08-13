5d63871f2b5409003deed46285693dfb423aa512
Mahdi-Erfan-Afshari <mahdiafshar2413@gmail.com>
2024-08-13 20:23:01 +0330

    update

--- app/api/podcasts/route.js
+++ app/api/podcasts/route.js
@@ -30,24 +30,24 @@ export async function PUT(request){
 
 
 
-// export async function POST(request){
-// 	const client = await clientPromise
-// 	let db = client.db('castplus')
-// 	const data = await request.json();
-// 	const { id } = data;
-// 	const { episodeId } = data;
-// 	let episode = await db.collection("podcasts").findOneAndUpdate(
-// 		{
-// 			id: id,
-// 		},
-// 		{ $set: { "episodes.$[element].sections.$[elem]" : data.sectionsList } },
-// 		{ arrayFilters: [
-// 			 {"element.id": episodeId},
-// 			 {"elem.id": data.sectionsList.id } 
-// 		  ] }
-// 	);
-// 	return NextResponse.json(episode)       
-// }
+export async function POST(request){
+	const client = await clientPromise
+	let db = client.db('castplus')
+	const data = await request.json();
+	const { id } = data;
+	const { episodeId } = data;
+	let episode = await db.collection("podcasts").findOneAndUpdate(
+		{
+			id: id,
+		},
+		{ $set: { "episodes.$[element].sections.$[elem]" : data.sectionsList } },
+		{ arrayFilters: [
+			 {"element.id": episodeId},
+			 {"elem.id": data.sectionsList.id } 
+		  ] }
+	);
+	return NextResponse.json(episode)       
+}
 
 // export async function PUT(request){
 // 	const client = await clientPromise
